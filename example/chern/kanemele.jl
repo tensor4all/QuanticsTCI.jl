f94c59682953d04505cad326b48c4fe87b876929
Ritter.Marc <Ritter.Marc@physik.uni-muenchen.de>
2023-02-06 14:20:00 +0100

    added Rashba term to Kane-Mele model

--- example/chern/kanemele.jl
+++ example/chern/kanemele.jl
@@ -3,6 +3,17 @@ function peierls(B::Real, i::HoneycombSite, j::HoneycombSite)
     rj = realspacecoordinates(j)
     phase = -B * (ri[1] - rj[1]) * (ri[2] + rj[2]) / 2
     return mod(phase, 2pi)
+    # return 0.0
+end
+
+function rashba(distance::Vector{Float64})
+    pauli = [
+        [0. 1.; 1. 0.],
+        [0. -1.0im; 1.0im 0.],
+        [1. 0.; 0. -1.]
+    ]
+
+    return pauli[1] .* distance[2] .- pauli[2] .* distance[1]
 end
 
 function get_Ht(
@@ -14,11 +25,12 @@ function get_Ht(
 )
     B = 4pi / sqrt(3) / q
     Ht = zeros(ComplexF64, 4q, 2, 4q, 2)
-    #Ht[:, 1, :, 1] += diagm(fill(mass, 4q))
 
     for (i, s) in enumerate(lattice.fromint)
+        # Alternating "mass term"
         Ht[i, 1, i, 1] += s.c == 1 ? mass : -mass
 
+        # Hopping
         for n in neighbours(s)
             rs, rn = realspacecoordinates.([s, n])
             phase = -k' * (rn - rs) + peierls(B, s, n)
@@ -27,13 +39,34 @@ function get_Ht(
             if !isnothing(derivative_direction)
                 result *= rs[derivative_direction] - rn[derivative_direction]
             end
-            # result += mass * exp(-1im * k' * (rn - rs))
 
             Ht[i, 1, j, 1] += result
         end
     end
     Ht[:, 2, :, 2] = Ht[:, 1, :, 1]
     return Ht
+end
+
+function get_HR(
+    q::Integer,
+    k::Vector{Float64},
+    lattice::TCI.IndexSet{HoneycombSite};
+    derivative_direction::Union{Nothing,Int}=nothing
+)
+    @assert isnothing(derivative_direction)
+
+    B = 4pi / sqrt(3) / q
+    HR = zeros(ComplexF64, 4q, 2, 4q, 2)
+
+    for (i, s) in enumerate(lattice.fromint)
+        for n in neighbours(s)
+            rs, rn = realspacecoordinates.([s, n])
+            j = TCI.pos(lattice, reducesite(n, 1, q))
+            HR[i, :, j, :] = 1im * exp(1im * peierls(B, s, n)) * rashba(rn - rs)
+        end
+    end
+
+    return HR
 end
 
 antisymmetricproduct(u, v) = u[1] * v[2] - u[2] * v[1]
@@ -46,6 +79,8 @@ function get_Hlambda(
 )
     B = 4pi / sqrt(3) / q
     Hlambda = zeros(ComplexF64, 4q, 2, 4q, 2)
+
+    # "Spin-orbit coupling" term
     for (i, s) in enumerate(lattice.fromint)
         for n in neighbours(s)
             for nn in neighbours(n)
@@ -67,12 +102,14 @@ end
 
 function get_H(
     q::Integer,
-    lambda::Float64,
+    lambda_SO::Float64,
+    lambda_R::Float64,
     k::Vector{Float64},
     lattice::TCI.IndexSet{HoneycombSite};
     mass::Float64=0.0,
     derivative_direction::Union{Nothing,Int}=nothing
 )
     return get_Ht(q, k, lattice; mass, derivative_direction) .+
-        lambda .* get_Hlambda(q, k, lattice; derivative_direction)
+        lambda_SO .* get_Hlambda(q, k, lattice; derivative_direction) .+
+        lambda_R .* get_HR(q, k, lattice)
 end
