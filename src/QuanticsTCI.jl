af64103d01df3b14f49d5d446d01e1eed07c9b49
Marc Ritter <ritter.marc@physik.uni-muenchen.de>
2023-03-24 20:08:46 +0100

    moved MPS utils to tciitensorconversion.jl

--- src/QuanticsTCI.jl
+++ src/QuanticsTCI.jl
@@ -7,17 +7,13 @@ using ITensors
 import LinearAlgebra: rank
 
 export qtt
-export qtt_to_mps
 export quantics_to_index, index_to_quantics
 export split_dimensions, merge_dimensions,
     interleave_dimensions, deinterleave_dimensions
 export binary_addition_mpo, binary_subtraction_mpo, kroneckerdelta_mpo
-export evaluate_mps
 
 include("quantics.jl")
 include("binaryops.jl")
 include("propagators.jl")
-include("xfac.jl")
-include("mps_util.jl")
 
 end
