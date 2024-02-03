7c410662fb2f8a9d54a16ea5adc04d6eb26df915
Hiroshi Shinaoka <h.shinaoka@gmail.com>
2024-02-03 12:38:45 +0900

    Removed unused code

--- src/QuanticsTCI.jl
+++ src/QuanticsTCI.jl
@@ -8,17 +8,9 @@ import LinearAlgebra: rank
 import QuanticsGrids as QG
 import QuanticsGrids: UnfoldingSchemes
 
-#export QuanticsFunction, QuanticsFunctionFused, QuanticsFunctionInterleaved
-#export UnfoldingSchemes
-#export quantics_to_index, index_to_quantics
-#export quantics_to_index_fused, index_to_quantics_fused
-#export quantics_to_index_interleaved, index_to_quantics_interleaved
-#export split_dimensions, merge_dimensions, fuse_dimensions,
-    #interleave_dimensions, deinterleave_dimensions
 export evaluate_mps
 export quanticscrossinterpolate
 
-#include("quantics.jl")
 include("tciinterface.jl")
 
 end
--- test/runtests.jl
+++ test/runtests.jl
@@ -2,5 +2,4 @@ using QuanticsTCI
 using Test
 using LinearAlgebra
 
-include("test_quantics.jl")
 include("test_tciinterface.jl")
