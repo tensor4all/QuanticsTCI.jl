766f047a87694f17b518e558b8d6575a1957246b
Hiroshi Shinaoka <h.shinaoka@gmail.com>
2025-05-15 15:06:43 +0900

    Determine underlying Integer type for CachedFunction and support up to UInt2048

--- src/QuanticsTCI.jl
+++ src/QuanticsTCI.jl
@@ -10,6 +10,11 @@ import Base: sum
 export quanticscrossinterpolate, evaluate, sum, integral
 export cachedata, quanticsfouriermpo
 
+import BitIntegers
+import BitIntegers: UInt256, UInt512, UInt1024
+
+BitIntegers.@define_integers 2048 MyInt2048 MyUInt2048
+
 include("tciinterface.jl")
 include("fouriertransform.jl")
 
